4e54b0bfed53c294e789832dff6b2c0bbf79dabd
SediqullahAzizyan <sediqullahazizyan1212@gmail.com>
2024-12-07 22:56:30 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+
+  
+  if (document.body.classList.contains('dark-mode')) {
+    themeToggle.textContent = '☀️';
+  } else {
+    themeToggle.textContent = '🌙';
+  }
+});
